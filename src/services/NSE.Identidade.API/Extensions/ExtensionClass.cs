1d868d70e73c367536a8ff22f8eec68b2f3c045c
Gabriel Julian <gluisjulian@gmail.com>
2024-01-03 21:36:26 -0300

    JWT

--- src/services/NSE.Identidade.API/Extensions/ExtensionClass.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace NSE.Identidade.API.Extensions
-{
-    public class ExtensionClass
-    {
-    }
-}
--- src/services/NSE.Identidade.API/Startup.cs
+++ src/services/NSE.Identidade.API/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -5,9 +6,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
 using System;
+using System.Text;
 
 namespace NSE.Identidade.API
 {
@@ -42,6 +46,35 @@ namespace NSE.Identidade.API
                             .AddDefaultTokenProviders();
 
 
+            //JWT
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptions =>
+            {
+                bearerOptions.RequireHttpsMetadata = true;
+                bearerOptions.SaveToken = true;
+                bearerOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor,
+                };
+            });
+
+
 
             services.AddControllers();
 
@@ -52,7 +85,7 @@ namespace NSE.Identidade.API
                 c.SwaggerDoc("v1", new OpenApiInfo
                 { 
                     Title = "NerdStore Enterprise Identity API",
-                    Description = "API faz parte do curso ASP.NET Core Enterprise",
+                    Description = "API faz parte do curso ASP.NET Core Enterprise - Continuar Aula 13",
                     Contact = new OpenApiContact() { Name = "Gabriel Julian", Email = "gluisjulian@gmail.com"},
                     License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/license/MIT")}
                 });
