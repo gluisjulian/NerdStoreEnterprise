4641a9c85f3c6cd5618eec6725520e56dc43757c
Gabriel Julian <gluisjulian@gmail.com>
2024-01-10 13:40:09 -0300

    Configurando Autenticação no MVC.

---
+++ src/mvc/NSE.WebApp.MVC/Configuration/IdentityConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static void AddIdentityConfiguration (this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login"; //Se nao tiver logado, encaminhar usuario para uma area da api
+                    options.AccessDeniedPath = "/acesso-negado"; //Se usuario nao tem acesso a x local da aplicação
+                });
+        }
+
+        public static void UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
