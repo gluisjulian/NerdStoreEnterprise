ad0bfc32e5c0ed7167df67b6ae0b6c1aecff2a9c
Gabriel Julian <gluisjulian@gmail.com>
2024-01-10 14:58:03 -0300

    Estrutura basica para comunicar com API.

--- src/mvc/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/mvc/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,13 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using static NSE.WebApp.MVC.Models.UsuarioViewModel;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
 
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            //Comunicar com API 
+            if (false) return View(usuarioRegistro);
+
+            //Realizar login na APP
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            //API - Login
+            if (false) return View(usuarioLogin);
+
+            //Realizar login na APP
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [HttpPost]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
